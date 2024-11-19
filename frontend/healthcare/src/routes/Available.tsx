f1a6a4c331bea2bfe8556bb92101a8c8b15bc676
Luis-Alvarezz <luisangelug10@gmail.com>
2024-11-18 20:50:03 -0600

    instalacion de dependencias big-calendar-react

--- frontend/healthcare/src/routes/Available.tsx
+++ frontend/healthcare/src/routes/Available.tsx
@@ -1,13 +1,22 @@
 import Header from "../layouts/Header";
+import { Calendar, momentLocalizer } from "react-big-calendar";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import moment from "moment";
 
-
+const localizer = momentLocalizer(moment);
 
 
 export default function Available() {
 
   return (
     <Header>
-      <div>Hola</div>
+      <div>
+      <Calendar
+        localizer={localizer}
+         startAccessor="start"
+         endAccessor="end"
+      />
+      </div>
     </Header>
   );
   
