5474bf448d7de34ef318e204afb5dabe5d37372b
Luis-Alvarezz <luisangelug10@gmail.com>
2024-11-21 08:15:23 -0600

    se añadio arreglo con los links para Sidebar Doctores

--- frontend/healthcare/src/data/SidebarData.ts
+++ frontend/healthcare/src/data/SidebarData.ts
@@ -1,15 +1,26 @@
-import { mdiViewDashboardOutline, mdiCalendarBlankOutline, mdiAccountCircleOutline, mdiHelpCircleOutline, mdiLogout } from '@mdi/js';
+import { mdiViewDashboardOutline, mdiCalendarBlankOutline, mdiAccountCircleOutline, mdiHelpCircleOutline, mdiLogout, mdiNoteMultipleOutline, mdiVideoOutline  } from '@mdi/js';
 
 import { LinksSidebar } from '../types/LinksSidebar';
 
 export const SidebarLinks : LinksSidebar = {
-    pacientes: {
-        links: [
-            {icono: mdiViewDashboardOutline, title: 'Dashboard', ruta: '/dashboard'},
-            {icono: mdiCalendarBlankOutline, title: 'Calendar', ruta: '/calendar'},
-            {icono: mdiAccountCircleOutline, title: 'Profile', ruta: '/profile'},
-            {icono: mdiHelpCircleOutline, title: 'Help', ruta: '/help'},
-            {icono: mdiLogout, title: 'Logout', ruta: '/logout'}
-        ]
-    }
+  pacientes: {
+    links: [
+        {icono: mdiViewDashboardOutline, title: 'Dashboard', ruta: '/dashboard'},
+        {icono: mdiCalendarBlankOutline, title: 'Calendar', ruta: '/available'},
+        {icono: mdiAccountCircleOutline, title: 'Profile', ruta: '/profile'},
+        {icono: mdiHelpCircleOutline, title: 'Help', ruta: '/help'},
+        {icono: mdiLogout, title: 'Logout', ruta: '/logout'}
+    ]
+  },
+  doctores: {
+    links: [
+      {icono: mdiViewDashboardOutline, title: 'Dashboard', ruta: '/dashboard'},
+      {icono: mdiNoteMultipleOutline, title: 'Patient Records', ruta: '/'},
+      {icono: mdiCalendarBlankOutline, title: 'My Availability', ruta: '/available'},
+      {icono: mdiAccountCircleOutline, title: 'My Consults', ruta: '/'},
+      {icono: mdiVideoOutline , title: 'Onlinea Consult', ruta: '/'},
+      {icono: mdiHelpCircleOutline, title: 'Help', ruta: '/'},
+      {icono: mdiLogout, title: 'Logout', ruta: '/logout'}
+    ]
+  }
 }
