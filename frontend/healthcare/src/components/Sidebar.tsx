458cbb8b3ee476053179a46776def1dbf0e98a3f
Mario Cobian <marvanis@hotmail.com>
2024-11-21 08:39:53 -0600

    Modificaciones del sidebar por que se elimino un div

--- frontend/healthcare/src/components/Sidebar.tsx
+++ frontend/healthcare/src/components/Sidebar.tsx
@@ -11,29 +11,31 @@ export default function Sidebar({menu}: {menu:boolean}) {
   return (
     <aside className={`md:flex md:flex-col border-slateBorder border w-60 items-center px-5 h-screen ${menu ? 'fixed flex flex-col' : 'hidden'}  md:relative bg-white z-[60] justify-end`}>
 
-        <div className='w-full'>
-          <div className="flex mt-11">
-              <img className='w-7 mr-1' src="./logo.svg" alt="logo" />
-              <h2 className='text-primary text-3xl'>Healthi</h2>
+        <div className='w-full h-full flex flex-col justify-between'>
+          <div>
+            <div className="flex mt-11">
+                <img className='w-7 mr-1' src="./logo.svg" alt="logo" />
+                <h2 className='text-primary text-3xl'>Healthi</h2>
+            </div>
+
+            <nav className="w-full mt-14">
+                <ul className='flex flex-col gap-3'>
+                  {SidebarLinks.pacientes.links.map((link) => (
+                    <li key={link.title}>
+                      <Link className={`flex items-center text-slate-500 p-3 rounded-md hover:bg-primary hover:text-white ${location === link.ruta ? 'bg-primary text-white' : ''}`} to={link.ruta}><Icon path={link.icono} size={'16px'} className='mr-2 hover:fill-white'/>{link.title}</Link>
+                    </li>
+                  ))}
+                </ul>
+
+                {/* <ul className='flex flex-col gap-5'>
+                  {SidebarLinks.doctores.links.map((link) => (
+                    <li key={link.title}>
+                      <Link className={`flex items-center text-slate-500 p-3 rounded-md hover:bg-primary hover:text-white ${location === link.ruta ? 'bg-primary text-white' : ''}`} to={link.ruta}><Icon path={link.icono} size={'16px'} className='mr-2 hover:fill-white'/>{link.title}</Link>
+                    </li>
+                  ))}
+                </ul> */}
+            </nav>
           </div>
-
-        <nav className="w-full mt-14">
-            <ul className='flex flex-col gap-3'>
-              {SidebarLinks.pacientes.links.map((link) => (
-                <li key={link.title}>
-                  <Link className={`flex items-center text-slate-500 p-3 rounded-md hover:bg-primary hover:text-white ${location === link.ruta ? 'bg-primary text-white' : ''}`} to={link.ruta}><Icon path={link.icono} size={'16px'} className='mr-2 hover:fill-white'/>{link.title}</Link>
-                </li>
-              ))}
-            </ul>
-
-            {/* <ul className='flex flex-col gap-5'>
-              {SidebarLinks.doctores.links.map((link) => (
-                <li key={link.title}>
-                  <Link className={`flex items-center text-slate-500 p-3 rounded-md hover:bg-primary hover:text-white ${location === link.ruta ? 'bg-primary text-white' : ''}`} to={link.ruta}><Icon path={link.icono} size={'16px'} className='mr-2 hover:fill-white'/>{link.title}</Link>
-                </li>
-              ))}
-            </ul> */}
-        </nav>
 
         <div className="mt-auto mb-5 relative">
           <img className='absolute bottom-24 left-4 w-10/12' src="./image_7.png" alt="img"/>
