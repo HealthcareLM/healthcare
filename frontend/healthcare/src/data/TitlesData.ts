5fa484c84d81ab64563db06e03ab7bc7c36de510
Luis-Alvarezz <luisangelug10@gmail.com>
2024-11-21 08:16:59 -0600

    Se implemento titulo para vista availability

--- frontend/healthcare/src/data/TitlesData.ts
+++ frontend/healthcare/src/data/TitlesData.ts
@@ -2,6 +2,7 @@
 
 export const titles : { [key: string]: string } = {
     '/dashboard': 'Welcome Back',
+    '/available': 'My Availability',
     '/profile': 'Profile',
     '/booking': 'Bookingk'
 }
