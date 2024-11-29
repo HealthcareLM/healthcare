43519aacae3bc6bf138facdda24075ed734213ee
Mario Cobian <marvanis@hotmail.com>
2024-11-28 23:18:53 -0600

    Modificacion a los interfaces

--- backend/src/interfaces/Usuarios.ts
+++ backend/src/interfaces/Usuarios.ts
@@ -1,12 +1,36 @@
-
-
-export interface IUsuario {
-    email: string,
-    telefono: string,
-    password: string,
-    birthdate: string,
-    rol: 'doctor' | 'paciente',
-    nombre?: string,
-    especialidad?: string,
-    costo?: string
+export interface IPaciente {
+    id?: string | undefined
+    email: string;
+    telefono: string;
+    password: string;
+    birthdate: string;
+    rol: 'paciente';
+    nombre: string;
 }
+
+export interface IDoctor {
+    id?: string | undefined
+    email: string;
+    telefono: string;
+    password: string;
+    birthdate: string;
+    rol: 'doctor';
+    nombre: string;
+    especialidad: string; // Obligatorio si es doctor
+    costo: string;        // Obligatorio si es doctor
+}
+
+// Unión discriminada
+export type IUsuario = IPaciente | IDoctor;
+
+
+// export interface IUsuario {
+//     email: string,
+//     telefono: string,
+//     password: string,
+//     birthdate: string,
+//     rol: 'doctor' | 'paciente',
+//     nombre?: string,
+//     especialidad?: string,
+//     costo?: string
+// }
